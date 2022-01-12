dc1ed25ded5cb6909d5bb6ae77504bf5168df147
Brennan <brennanwilliams2000@gmail.com>
2022-01-12 10:33:57 -0700

    setup mvc

--- MyFirstASPNETWEbApp/Startup.cs
+++ MyFirstASPNETWEbApp/Startup.cs
@@ -16,13 +16,27 @@ namespace MyFirstASPNETWEbApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDefaultFiles();
+            if (env.IsEnvironment("Development"))
+            {
+                app.UseDeveloperExceptionPage();
+            }
+    
+            app.UseRouting();
             app.UseStaticFiles();
+            app.UseEndpoints(
+                endpoints =>
+                {
+                    endpoints.MapControllerRoute(
+                        name: "home",
+                        pattern: "{controller=home}/{action=Index}/{id?}"
+                        );
+                });
         }
     }
 }
